cf1138e53482ad13c6ed2e836089578064423e20
d-IT-tO <bykovivan073@gmail.com>
2025-11-04 22:41:51 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,60 @@
-#include <coordinates_decoder.h>
+#include "coordinates_decoder.h"
+#include "telemetry_analyzer.h"
 
-int main() {
+void navigation_decoder() {
     int src, dest;
-    char* num = (char*)malloc(32 * sizeof(char));
+    char num[32];
     scanf("%d %d %31s", &src, &dest, num);
 
     char* result = student1_process(src, dest, num);
     if (result) {
-        printf("Результат: %s\n", result);
+        printf("Result: %s\n", result);
         free(result);
     }
+}
+
+void analyzer(){
+    int cnt;
+    puts("write the number of numbers");
+    scanf("%d", &cnt);
+    if (cnt < 1){
+        puts("error");
+        return;
+    }   
+    char** arr = malloc(cnt * sizeof(char*));
+    int* base = malloc(cnt * sizeof(int));
+    if (arr == NULL || base == NULL)
+        puts("malloc error");
+    else{
+        puts("Write the number and base by a space");
+        for (int i = 0; i < cnt; i++)
+            scanf("%s%d", (arr + i), (base + i));
+        arr_full_analyze(arr, base, cnt);
+    }
+    free(arr);
+    free(base);
     return 0;
 }
 
+int main(){
+    int choice;
+    puts("Welcome to Intergalactic-Data-Processing-Center\n
+        choose your action\n
+        1. Navigation coordinates decoder\n
+        2. Telemetry Analyzer\n
+        0. exit");
+    while (1):
+        switch(scanf("%d", &choice)){
+            case 1:
+                navigation_decoder();
+                puts("Navigation coordinates decoder end");
+                continue;
+            case 2:
+                analyzer();
+                puts("Telemetry Analyzer end");
+                continue;
+            case 0:
+                puts("program end");
+                return 0;
+    }
+}
