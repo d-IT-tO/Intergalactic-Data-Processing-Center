4ca34c43525309f6fcd55d2449e0d5f674d038bc
d-IT-tO <bykovivan073@gmail.com>
2025-11-04 20:57:07 +0700

    Update and replace coordinates_decoder.h

--- include/coordinates_decoder.h
+++ include/coordinates_decoder.h
@@ -1,25 +1,25 @@
-#ifndef PERDUN_H
-#define PERDUN_H
+#ifndef COORDINATES_DECODER_H
+#define COORDINATES_DECODER_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-//Валидация
+//validation
 int validate_base(int base);
 int validate_number(const char* number, int base);
 
-//Преобразования
+//transformations
 double string_to_decimal(const char* number, int base);
 char* decimal_to_string(double number, int base, int precision);
 
-//Вспомогательные функции
+//auxiliary functions
 int char_to_value(char c);
 char value_to_char(int value);
 void split_number_string(const char* number, char* integer_part, char* fractional_part);
 
-//Главная функция
+//main function
 char* student1_process(int src_base, int dest_base, const char* number);
 
 #endif
