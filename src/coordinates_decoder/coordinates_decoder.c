22fe5bbce0082a1d9a35a0940b89628dc7659463
a517730598 <semenov.art20@gmail.com>
2025-11-04 21:33:12 +0700

    Update coordinates_decoder.c

--- src/coordinates_decoder/coordinates_decoder.c
+++ src/coordinates_decoder/coordinates_decoder.c
@@ -141,16 +141,3 @@ char* student1_process(int src_base, int dest_base, const char* number) {
     return result;
 }
 
-int main() {
-    int src, dest;
-    char num[32];
-    scanf("%d %d %31s", &src, &dest, num);
-
-    char* result = student1_process(src, dest, num);
-    if (result) {
-        printf("Результат: %s\n", result);
-        free(result);
-    }
-    return 0;
-
-}
