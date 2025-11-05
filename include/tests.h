692d02976ae43f14e395956aa52c8db2c541d9dc
a517730598 <semenov.art20@gmail.com>
2025-11-05 08:56:31 +0700

    Create tests.h

---
+++ include/tests.h
@@ -0,0 +1,16 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <string.h>
+#include <stdlib.h>
+
+void test_convertations(void);
+void test_fractional(void);
+void test_blya_nu_tut_ne_ebu(void);
+void test_err(void);
+
+#endif
